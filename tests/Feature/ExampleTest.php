bbe3f0fdb03612b1c8465bc2034b27c677851a91
lzy <497066369@qq.com>
2026-06-22 15:02:47 +0800

    chore: release 3.6.2

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,7 +18,7 @@ class ExampleTest extends TestCase
 
         $response
             ->assertJsonPath('service', '2026-hpe-api')
-            ->assertJsonPath('version', '3.6.1')
+            ->assertJsonPath('version', '3.6.2')
             ->assertJsonPath('database.driver', 'mysql')
             ->assertJsonStructure([
                 'status',
