5c1ef23367d4720a30a86c09d3c5e6ee873d466c
lzy <497066369@qq.com>
2026-06-30 17:38:16 +0800

    chore: release backend version 3.6.8

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
         return response()->json([
             'status' => 'ok',
             'service' => '2026-hpe-api',
-            'version' => '3.6.7',
+            'version' => '3.6.8',
             'database' => [
                 'driver' => 'mysql',
                 'connected' => true,
@@ -25,7 +25,7 @@ Route::get('/', function () {
         return response()->json([
             'status' => 'error',
             'service' => '2026-hpe-api',
-            'version' => '3.6.7',
+            'version' => '3.6.8',
             'database' => [
                 'driver' => 'mysql',
                 'connected' => false,
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,7 +18,7 @@ class ExampleTest extends TestCase
 
         $response
             ->assertJsonPath('service', '2026-hpe-api')
-            ->assertJsonPath('version', '3.6.7')
+            ->assertJsonPath('version', '3.6.8')
             ->assertJsonPath('database.driver', 'mysql')
             ->assertJsonStructure([
                 'status',
